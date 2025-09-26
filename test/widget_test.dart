7199fd7e5ea73d810d8a80fe2b3bf6590ba12f8a
zidnafaz <zidnafaza27@gmail.com>
2025-09-26 14:50:38 +0700

    Update import statement to reflect project name change from pemrograman_mobile to moneta

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pemrograman_mobile/main.dart';
+import 'package:moneta/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
